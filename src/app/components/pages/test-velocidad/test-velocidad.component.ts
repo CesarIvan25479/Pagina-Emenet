b7775c23b758e9b243dbbdabd32d1cce9dc3ee16
Maximo132 <maxfriday16@gmail.com>
2025-10-30 13:46:09 -0600

    feat: implementar página de prueba de velocidad con diseño responsivo y animaciones

--- src/app/components/pages/test-velocidad/test-velocidad.component.ts
+++ src/app/components/pages/test-velocidad/test-velocidad.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 import { PreloaderService } from '../../../services/preloader.service';
+import { WebglLiquidComponent } from '../../shared/webgl-liquid/webgl-liquid.component';
 @Component({
   selector: 'app-test-velocidad',
   standalone: true,
   imports: [
     CommonModule,
-    AnimateOnScrollModule
+    AnimateOnScrollModule,
+    WebglLiquidComponent
   ],
   templateUrl: './test-velocidad.component.html',
 })
