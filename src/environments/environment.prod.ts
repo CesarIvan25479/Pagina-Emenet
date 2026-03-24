cea79582e736c1cf098889d513ef4cf3a521caa8
Cesar Rivera <cesarivan25479@gmail.com>
2026-03-23 18:01:12 -0600

    ignorar enviroment

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://isp-back.emenet.mx/api',
-  tokenClients: 'wDApSmjJxmYA9Tf2fgSV15gfuYLe5au4gCZA7zGwegE25IVLWeWFo9dCUugVrGYY',
-  pagoraliaUrl: "https://emenet.pagoralia.live/api",
-  tokenPagoralia: "f188b00c25d172a33a4a75c6006e404a539838816afbecc4a6819025d29a15a3"
-};
